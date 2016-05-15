25f9293441b1fc5dd62b998b113c682a4ffaefaf
pablo@codice.es <>
2016-05-15 14:46:15 +0200

    Used a custom scheduler from
    Got very similar results to the original threadPool thing: when the asyncs are awakened, they go to new threads, although it is clear that now most of the stuff is done on the same thread.
    
    Type a name and then ENTER to schedule a new request
    [req0] - [thId: 6] - Starting request
    [req0] - [thId: 6] - Do heavy calculation ...
    [req0] - [thId: 6] - Done. 1000 ms
    [req0] - [thId: 6] - Sleeping to do async
    [req1] - [thId: 6] - Starting request
    [req1] - [thId: 6] - Do heavy calculation ...
    [req1] - [thId: 6] - Done. 1016 ms
    [req1] - [thId: 6] - Sleeping to do async
    [req0] - [thId: 11] - Big async operation done
    [req1] - [thId: 13] - Big async operation done

--- Program.cs
+++ Program.cs
@@ -11,34 +11,22 @@ namespace TestAsyncProcessing
     {
         static void Main(string[] args)
         {
-            ThreadPool.SetMaxThreads(1, 1);
+            LimitedConcurrencyLevelTaskScheduler lcts = new LimitedConcurrencyLevelTaskScheduler(1);
+            TaskFactory factory = new TaskFactory(lcts);
 
-            Task.Factory.StartNew(() =>
-                {
-                    Request t = new Request("req0");
-
-                    t.ProcessRequest("req0");
-                }
-            );
-
-            Task.Factory.StartNew(() =>
-                {
-                    Request t = new Request("req1");
-
-                    t.ProcessRequest("req1");
-                }
-            );
+            factory.StartNew(() => { new Request("req0").ProcessRequest(); } );
+            factory.StartNew(() => { new Request("req1").ProcessRequest(); });
 
             while (true)
             {
                 Console.WriteLine("Type a name and then ENTER to schedule a new request");
                 string order = Console.ReadLine();
 
-                Task.Factory.StartNew(() =>
+                factory.StartNew(() =>
                     {
                         Request t = new Request(order);
 
-                        t.ProcessRequest(order);
+                        t.ProcessRequest();
                     }
                 );
             }
@@ -51,23 +39,23 @@ namespace TestAsyncProcessing
                 mName = name;
             }
 
-            internal async void ProcessRequest(string name)
+            internal async void ProcessRequest()
             {
-                WriteLine(name, "Starting request");
+                WriteLine(mName, "Starting request");
 
                 int ini = Environment.TickCount;
 
-                WriteLine(name, "Do heavy calculation ...");
+                WriteLine(mName, "Do heavy calculation ...");
 
                 Thread.Sleep(1000);
 
-                WriteLine(name, "Done. {0} ms", Environment.TickCount - ini);
+                WriteLine(mName, "Done. {0} ms", Environment.TickCount - ini);
 
-                WriteLine(name, "Sleeping to do async");
+                WriteLine(mName, "Sleeping to do async");
 
                 await Task.Delay(10000);
 
-                WriteLine(name, "Big async operation done");
+                WriteLine(mName, "Big async operation done");
             }
 
             void WriteLine(string requestName, string format, params object[] args)
@@ -79,6 +67,136 @@ namespace TestAsyncProcessing
             }
 
             string mName;
+        }
+
+        /// <summary>
+        /// Copied from https://msdn.microsoft.com/en-us/library/ee789351(v=vs.100).aspx
+        /// Provides a task scheduler that ensures a maximum concurrency level while
+        /// running on top of the ThreadPool.
+        /// </summary>
+        public class LimitedConcurrencyLevelTaskScheduler : TaskScheduler
+        {
+            /// <summary>Whether the current thread is processing work items.</summary>
+            [ThreadStatic]
+            private static bool _currentThreadIsProcessingItems;
+            /// <summary>The list of tasks to be executed.</summary>
+            private readonly LinkedList<Task> _tasks = new LinkedList<Task>(); // protected by lock(_tasks)
+            /// <summary>The maximum concurrency level allowed by this scheduler.</summary>
+            private readonly int _maxDegreeOfParallelism;
+            /// <summary>Whether the scheduler is currently processing work items.</summary>
+            private int _delegatesQueuedOrRunning = 0; // protected by lock(_tasks)
+
+            /// <summary>
+            /// Initializes an instance of the LimitedConcurrencyLevelTaskScheduler class with the
+            /// specified degree of parallelism.
+            /// </summary>
+            /// <param name="maxDegreeOfParallelism">The maximum degree of parallelism provided by this scheduler.</param>
+            public LimitedConcurrencyLevelTaskScheduler(int maxDegreeOfParallelism)
+            {
+                if (maxDegreeOfParallelism < 1) throw new ArgumentOutOfRangeException("maxDegreeOfParallelism");
+                _maxDegreeOfParallelism = maxDegreeOfParallelism;
+            }
+
+            /// <summary>Queues a task to the scheduler.</summary>
+            /// <param name="task">The task to be queued.</param>
+            protected sealed override void QueueTask(Task task)
+            {
+                // Add the task to the list of tasks to be processed.  If there aren't enough
+                // delegates currently queued or running to process tasks, schedule another.
+                lock (_tasks)
+                {
+                    _tasks.AddLast(task);
+                    if (_delegatesQueuedOrRunning < _maxDegreeOfParallelism)
+                    {
+                        ++_delegatesQueuedOrRunning;
+                        NotifyThreadPoolOfPendingWork();
+                    }
+                }
+            }
+
+            /// <summary>
+            /// Informs the ThreadPool that there's work to be executed for this scheduler.
+            /// </summary>
+            private void NotifyThreadPoolOfPendingWork()
+            {
+                ThreadPool.UnsafeQueueUserWorkItem(_ =>
+                {
+                    // Note that the current thread is now processing work items.
+                    // This is necessary to enable inlining of tasks into this thread.
+                    _currentThreadIsProcessingItems = true;
+                    try
+                    {
+                        // Process all available items in the queue.
+                        while (true)
+                        {
+                            Task item;
+                            lock (_tasks)
+                            {
+                                // When there are no more items to be processed,
+                                // note that we're done processing, and get out.
+                                if (_tasks.Count == 0)
+                                {
+                                    --_delegatesQueuedOrRunning;
+                                    break;
+                                }
+
+                                // Get the next item from the queue
+                                item = _tasks.First.Value;
+                                _tasks.RemoveFirst();
+                            }
+
+                            // Execute the task we pulled out of the queue
+                            base.TryExecuteTask(item);
+                        }
+                    }
+                    // We're done processing items on the current thread
+                    finally { _currentThreadIsProcessingItems = false; }
+                }, null);
+            }
+
+            /// <summary>Attempts to execute the specified task on the current thread.</summary>
+            /// <param name="task">The task to be executed.</param>
+            /// <param name="taskWasPreviouslyQueued"></param>
+            /// <returns>Whether the task could be executed on the current thread.</returns>
+            protected sealed override bool TryExecuteTaskInline(Task task, bool taskWasPreviouslyQueued)
+            {
+                // If this thread isn't already processing a task, we don't support inlining
+                if (!_currentThreadIsProcessingItems) return false;
+
+                // If the task was previously queued, remove it from the queue
+                if (taskWasPreviouslyQueued) TryDequeue(task);
+
+                // Try to run the task.
+                return base.TryExecuteTask(task);
+            }
+
+            /// <summary>Attempts to remove a previously scheduled task from the scheduler.</summary>
+            /// <param name="task">The task to be removed.</param>
+            /// <returns>Whether the task could be found and removed.</returns>
+            protected sealed override bool TryDequeue(Task task)
+            {
+                lock (_tasks) return _tasks.Remove(task);
+            }
+
+            /// <summary>Gets the maximum concurrency level supported by this scheduler.</summary>
+            public sealed override int MaximumConcurrencyLevel { get { return _maxDegreeOfParallelism; } }
+
+            /// <summary>Gets an enumerable of the tasks currently scheduled on this scheduler.</summary>
+            /// <returns>An enumerable of the tasks currently scheduled.</returns>
+            protected sealed override IEnumerable<Task> GetScheduledTasks()
+            {
+                bool lockTaken = false;
+                try
+                {
+                    Monitor.TryEnter(_tasks, ref lockTaken);
+                    if (lockTaken) return _tasks.ToArray();
+                    else throw new NotSupportedException();
+                }
+                finally
+                {
+                    if (lockTaken) Monitor.Exit(_tasks);
+                }
+            }
         }
     }
 }
